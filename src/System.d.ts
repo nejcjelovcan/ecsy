2ee42045c012528c747a497fe556552f754c8b96
Nejc Jelovčan <nejc.jelovcan@gmail.com>
2019-12-12 12:54:26 +0100

    Updated System typescript definitions: - made System generic <T extends World> - added .world:T instance variable - added .queries instance variable

--- src/System.d.ts
+++ src/System.d.ts
@@ -1,13 +1,33 @@
 import {Component, ComponentConstructor} from "./Component";
+import {World} from './World'
+import {Entity} from './Entity'
 
 /**
  * A system that manipulates entities in the world.
  */
-export abstract class System {
+export abstract class System<T extends World = World> {
   /**
    * Whether the system will execute during the world tick.
    */
   enabled:boolean;
+
+  /**
+   * World instance
+   */
+  world:T;
+
+  /**
+   * Queries
+   */
+  queries: {
+    [key: string]: {
+      results: Entity[]
+      added: Entity[]
+      removed: Entity[]
+      changed: Entity[]
+    }
+  }
+
   /**
    * Resume execution of this system.
    */
@@ -17,6 +37,7 @@ export abstract class System {
    * Stop execution of this system.
    */
   stop():void
+
 }
 
 export interface SystemConstructor<T extends System> {
